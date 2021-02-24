5f6fd4bd1e53c6f5b2bd75f7f44302b5d6783929
Tacklbac <small.rick.y@gmail.com>
2021-02-24 22:55:02 +0000

    Added method to test counter

--- src/main/java/ie/gmit/Counter.java
+++ src/main/java/ie/gmit/Counter.java
@@ -1,4 +1,15 @@
 package ie.gmit;
 
 public class Counter {
+	private int count;
+
+	public Counter()
+	{
+		this.count = 0;
+	}
+
+	public int getCount()
+	{
+		return count;
+	}
 }
--- src/test/java/ie/gmit/CounterTest.java
+++ src/test/java/ie/gmit/CounterTest.java
@@ -1,6 +1,23 @@
+/*
+Name: Ricky Small
+Email: g00352166@gmit.ie
+Purpose: Generate a counter app // Junit Lab Example
+ */
+
 package ie.gmit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CounterTest {
 
+	private Counter myCounter;
+	@Test
+	void TestConstructor()
+	{
+		myCounter = new Counter();
+		assertEquals(0, myCounter.getCount());
+	}
 
 }
